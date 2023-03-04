584b226ba8a1f40cce9a175466aefd3a783e57ce
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-04 11:07:01 -0300

    creo un handlechange para manejar el estado de todos los inputs

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,7 +1,106 @@
+import { useState } from "react";
+
 export default function Create (){
+
+    const [title,setTitle]= useState('');
+    const [author,setAuthor]= useState('');
+    const [cover,setCover]= useState('');
+    const [intro,setIntro]= useState('');
+    const [completed,setCompleted]= useState('false');
+    const [review,setReview]= useState('');
+
+    //con un solo handlechange manejo el estado de todos los inputs
+    function handleChange(e){
+        const name= e.target.name;
+        const value=e.target.value;
+
+        switch (name){
+            case 'title':
+                setTitle(value);
+                break;
+            
+            case 'intro':
+                setIntro(value);
+                break;
+                
+            case 'completed':
+                setCompleted(e.target.checked);
+                break;
+
+            case 'review':
+                setReview(value);
+                break;
+
+                default:
+        }
+
+    }
+
+    function handleOnChangeFile(e) {
+
+        
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        const newBook ={
+            id: crypto.randomUUID(),
+            title,
+            author,
+            cover,
+            intro,
+            completed,
+            review
+        };
+
+        //TODO: mandar a registrar libro
+    }
+
     return (
         <div>
-            Create
+            <form onSubmit={handleSubmit}>
+                <div>
+
+                    <div>Title</div>
+                    <input type="text" name="title" onChange={handleChange} value={title} />
+                </div>
+
+                <div>
+
+                    <div>Author</div>
+                    <input type="text" name="author" onChange={handleChange} value={author} />
+                </div>
+
+                
+                <div>
+
+                    <div>Cover</div>
+                    <input type="file" name="cover" onChange={handleOnChangeFile} />
+                    <div></div>
+                </div>
+
+                <div>
+
+                <div>Introduction</div>
+                <input type="text" name="intro" onChange={handleChange} value={intro} />
+                </div>
+
+                <div>
+
+                <div>Completed</div>
+                <input type="checkbox" name="completed" onChange={handleChange} value={completed} />
+                </div>
+
+
+                <div>
+
+                <div>Review</div>
+                <input type="text" name="review" onChange={handleChange} value={review} />
+                </div>
+
+                <input type="submit" value="Register book"></input>
+            </form>
         </div>
     )
 }
